48ad1f3acfb28375971bcced267ba7962f8577ca
TheArcx <daljit.josh@gmail.com>
2021-12-24 10:36:07 -0500

    Update passport.js
    - Query the db for an existing user
    - if not then save and add that user

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy; // Instruct passport
 const keys = require('../config/keys'); // Object with keys
+const mongoose = require('mongoose');
+
+// pull schema out of mongoose
+const User = mongoose.model('users');
 
 // googlestrategy can be accessed with 'google'
 passport.use(new GoogleStrategy(
@@ -12,9 +16,18 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken,refreshToken,profile, done) => { // take information recieved from google, create in database
-            console.log('access token', accessToken);
-            console.log('refreshtoken',refreshToken);
-            console.log('profile:',profile);
+            User.findOne({ googleId: profile.id }) // Query the DB
+                .then((existingUser) => { // if no user then existingUser = null
+                    if(existingUser){
+                        // Record already exists
+                        done(null, existingUser);
+                    }else{
+                        // Record does not exist
+                        new User({ googleId: profile.id}) // modal instance
+                            .save() // save
+                            .then(user => done(null,user)); // wait on completion
+                    }
+                })
         }
     )
 );
