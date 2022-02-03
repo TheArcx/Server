a2233751f7b4a13e2594fc0d09d4074302f0b3d3
TheArcx <daljit.josh@gmail.com>
2022-02-03 10:20:06 -0500

    Mailer updates
    - Mailer object updates with sendgrid

--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const checkLogin = require('../middlewares/checkLogin');
 const requireCredit = require('../middlewares/requireCredit');
+const Mailer = require('../services/mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -16,5 +18,7 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
+        
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
