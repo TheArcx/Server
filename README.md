f0f74b5776f68e15fce47f4bed41c0b638865114
TheArcx <daljit.josh@gmail.com>
2022-03-22 21:17:30 -0400

    V3.0
    - Email responses saving to database
    - Added redux-form for creating initial surveys + related components
    - Updated readme
    - Updated routing

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # Emailer
+
 The purpose of this application is to be a survey software in which people can pay credits to send out simple surveys (yes/no answers) to recipients and gather the results.
+
 # Run
+
 The package.json file contains all the custom run scripts made for the application. \
 npm run dev - Runs the application in developer mode.
+
 ## Deploying to Heroku
+
 npm run build - Builds the application and gets it ready to deploy to Heroku. The github repo is already linked to it in the Heroku backend.
 
 # Technologies used
+
 - NodeJS
 - React
 - Redux
 - Express
 - Passport
 - MongoDB
+- MaterializeCSS Designs
 - Stripe
--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -7,6 +7,11 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    });
+    
     app.post('/api/surveys', checkLogin, requireCredit, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
@@ -20,7 +25,17 @@ module.exports = app => {
         });
         
         const mailer = new Mailer(survey, surveyTemplate(survey));
-        await mailer.send();
-        await survey.save();
+
+        try {
+            await mailer.send();
+            await survey.save();
+    
+            req.user.credits -= 1; // Adjust credits
+            const user = await req.user.save();
+    
+            res.send(user); // Update user model   
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
