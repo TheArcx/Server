9a37dc375999bf2a07690331c6fac15dfc225306
TheArcx <daljit.josh@gmail.com>
2022-03-23 21:59:26 -0400

    Survey Creation Pages
    - Added reactForms for survey creation
    - CSS styling
    - Working on error messages for incorrect inputs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,17 +17,15 @@ class App extends Component {
     }
 
     render(){
-        return(
-            <div className = "container">
-                <BrowserRouter>
-                    <div>
-                        <Header />
-                        <Route exact path = "/" component={Landing} />
-                        <Route exact path = "/surveys" component={Dashboard} />
-                        <Route path = "/surveys/new" component={SurveyNew} />
-                    </div>
-                </BrowserRouter>
-            </div>
+        return (
+            <BrowserRouter>
+                <div className = "container">
+                    <Header />
+                    <Route exact path = "/" component={Landing} />
+                    <Route exact path = "/surveys" component={Dashboard} />
+                    <Route path = "/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         );
     }
 }
