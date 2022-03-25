e5cfeba7c8e0875bea4c58f58c0130450e7bcc17
TheArcx <daljit.josh@gmail.com>
2022-03-25 13:35:35 -0400

    Update SurveyNew.js
    - added toggle between surveyreview and surveyform

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,30 @@
-// SurveyNew shows SurveyForm
+// SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
-
+import SurveyReview from "./SurveyFormReview";
 class SurveyNew extends Component {
+    state = { showReview: false };
+
+    renderContent() {
+        if (this.state.showReview === true) {
+            return (
+                <SurveyReview
+                    onCancel={() => this.setState({ showReview: false })}
+                />
+            );
+        }
+
+        return (
+            <SurveyForm
+                onSurveySubmit={() => this.setState({ showReview: true })}
+            />
+        );
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm />
+                { this.renderContent() }
             </div>
         );
     }
