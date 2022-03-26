6dea1e0ae2c088952d325a7e4e03f61ac852709d
TheArcx <daljit.josh@gmail.com>
2022-03-26 14:59:49 -0400

    Updating Variables
    - changes emails property to recipients

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
     { label: 'Survey Title', name: 'title', noValueError: 'Provide a title'},
     { label: 'Subject Line', name: 'subject', noValueError: 'Provide a subject'},
     { label: 'Email Body', name: 'body', noValueError: 'Provide an email body'},
-    { label: 'Recipient List', name: 'emails', noValueError: 'Provide a recipient list'}
+    { label: 'Recipient List', name: 'recipients', noValueError: 'Provide a recipient list'}
 ];
