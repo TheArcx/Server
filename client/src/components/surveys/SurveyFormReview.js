d61e7642f36172cee87faec196c1cba2ae59836e
TheArcx <daljit.josh@gmail.com>
2022-03-26 15:01:11 -0400

    Update SurveyFormReview.js
    - connected withRouter to formreview
    - survey submit button routes to dashboard

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,9 +3,10 @@ import _ from 'lodash';
 import React from 'react';
 import FIELDS from './formFields';
 import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import { withRouter } from 'react-router-dom';
 
-
-const SurveyReview = ({ onCancel, formValues }) => {
+const SurveyReview = ({ onCancel, formValues, submitSurvey, history}) => {
     
     const reviewFields = _.map(FIELDS, ({ name, label}) => {
         return (
@@ -23,12 +24,18 @@ const SurveyReview = ({ onCancel, formValues }) => {
             <h5>Please confirm your entries</h5>
             {reviewFields}
             <button
-                className="yellow darken-3 btn-flat"
+                className="yellow darken-3 white-text btn-flat"
                 onClick={onCancel}
             >
                 Back
             </button>
-
+            <button
+                className="green white-text btn-flat right"
+                onClick={() => submitSurvey(formValues, history)}
+            >
+                Send Survey
+                <i className="material-icons right">email</i>
+            </button>
         </div>
     );
 };
@@ -39,4 +46,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(SurveyReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyReview));
